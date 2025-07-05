8c6606b3f7d6e7911954110d0d68c57c385a0c27
James Taylor <soonermagic32@gmail.com>
2025-07-05 09:25:35 -0400

    Added menu

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,24 @@
 extern crate xplm;
 
+use xplm::menu::{ActionItem, Menu, MenuClickHandler};
 use xplm::plugin::{Plugin, PluginInfo};
 use xplm::{debugln, xplane_plugin};
 
-struct FlightStreamPlugin;
+struct FlightStreamPlugin {
+    _plugins_submenu: Menu,
+}
 
 impl Plugin for FlightStreamPlugin {
     type Error = std::convert::Infallible;
 
     fn start() -> Result<Self, Self::Error> {
-        debugln!("Hello, World! From minimal plugin");
-        Ok(FlightStreamPlugin)
+        let plugin_submenu = Menu::new("Flightstream-rs").unwrap();
+        plugin_submenu.add_child(ActionItem::new("Download and Load Flight Plan", DownloadAndLoadHandler).unwrap());
+        plugin_submenu.add_child(ActionItem::new("Set username", SetUserNameHandler).unwrap());
+        plugin_submenu.add_to_plugins_menu();
+        Ok(FlightStreamPlugin {
+            _plugins_submenu: plugin_submenu
+        })
     }
 
     fn info(&self) -> PluginInfo {
@@ -22,3 +30,19 @@ impl Plugin for FlightStreamPlugin {
 }
 
 xplane_plugin!(FlightStreamPlugin);
+
+struct DownloadAndLoadHandler;
+
+impl MenuClickHandler for DownloadAndLoadHandler {
+    fn item_clicked(&mut self, _item: &ActionItem) {
+        debugln!("Download Selected");
+    }
+}
+
+struct SetUserNameHandler;
+
+impl MenuClickHandler for SetUserNameHandler {
+    fn item_clicked(&mut self, _item: &ActionItem) {
+        debugln!("Set Username Selected");
+    }
+}
